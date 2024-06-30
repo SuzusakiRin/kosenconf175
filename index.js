6e76de744d42085d02dd7b234c5b420a1ae009d0
鈴崎 燐 <167955053+SuzusakiRin@users.noreply.github.com>
2024-06-30 22:09:47 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const text = "Reflection of the year, And into the future.";
     const outputElement = document.getElementById('output');
     let index = 0;
-    const delay = 100; // 文字を表示する間隔（ミリ秒）
+    const delay = 40; // 文字を表示する間隔（ミリ秒）
 
     function displayNextCharacter() {
         if (index < text.length) {
